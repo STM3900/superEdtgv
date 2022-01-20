2675326742c04f73e41c1b4bc93cd216f3128899
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2022-01-20 20:34:25 +0100

    CacheClass composant

---
+++ components/CacheCheck.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="cache-class">
+    <label for="cacheCheckbox">Désactiver le cache</label>
+    <input
+      type="checkbox"
+      id="cacheCheckbox"
+      :class="getSelectedColor.class"
+      v-model="cacheActivateCheckbox"
+      @change="saveCache()"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "CacheCheck",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["getCacheActivate", "getSelectedColor"]),
+    cacheActivateCheckbox: {
+      get() {
+        return this.getCacheActivate;
+      },
+      set(status) {
+        this.changeCacheActivate(status);
+      },
+    },
+  },
+  mounted() {
+    this.changeCacheActivate(
+      JSON.parse(localStorage.getItem("userCache")) ?? false
+    );
+  },
+  methods: {
+    ...mapActions(["changeCacheActivate"]),
+    saveCache() {
+      localStorage.setItem("userCache", this.getCacheActivate);
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* pour la checkbox */
+.cache-class {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: center;
+}
+
+.cache-class label {
+  font-family: "Quicksand", sans-serif;
+  margin: 0;
+  font-size: 14px;
+  margin-right: 5px;
+}
+
+.cache-class input[type="checkbox"] {
+  -webkit-appearance: none;
+  margin: 0;
+  margin-right: 3px;
+
+  padding: 7px;
+  border-radius: 4px;
+
+  display: inline-block;
+  position: relative;
+  transition: 0.3s;
+}
+
+.cache-class input[type="checkbox"]:hover {
+  cursor: pointer;
+}
+
+.cache-class .color1 {
+  border: 1px solid hsl(0, 70%, 75%);
+}
+
+.cache-class .color1:checked {
+  background-color: hsl(0, 70%, 85%);
+}
+
+.cache-class .color2 {
+  border: 1px solid hsl(60, 70%, 75%);
+}
+
+.cache-class .color2:checked {
+  background-color: hsl(60, 70%, 85%);
+}
+
+.cache-class .color3 {
+  border: 1px solid hsl(100, 70%, 75%);
+}
+
+.cache-class .color3:checked {
+  background-color: hsl(100, 70%, 85%);
+}
+
+.cache-class .color4 {
+  border: 1px solid hsl(170, 70%, 75%);
+}
+
+.cache-class .color4:checked {
+  background-color: hsl(170, 70%, 85%);
+}
+
+.cache-class .color5 {
+  border: 1px solid hsl(200, 70%, 75%);
+}
+
+.cache-class .color5:checked {
+  background-color: hsl(200, 70%, 85%);
+}
+
+.cache-class .color6 {
+  border: 1px solid hsl(250, 70%, 75%);
+}
+
+.cache-class .color6:checked {
+  background-color: hsl(250, 70%, 85%);
+}
+
+.cache-class .color7 {
+  border: 1px solid hsl(330, 70%, 75%);
+}
+
+.cache-class .color7:checked {
+  background-color: hsl(330, 70%, 85%);
+}
+
+.cache-class input[type="checkbox"]:checked:after {
+  content: ""; /* \2714 */
+  font-size: 8px;
+  position: absolute;
+  top: 0px;
+  left: 3px;
+  color: white;
+}
+
+/* */
+</style>
--- components/PersonForm.vue
+++ components/PersonForm.vue
@@ -70,16 +70,7 @@
 
     <div class="color-wrapper">
       <ColorChanger />
-      <div class="cache-class">
-        <label for="cacheCheckbox">Désactiver le cache</label>
-        <input
-          type="checkbox"
-          id="cacheCheckbox"
-          :class="getSelectedColor.class"
-          v-model="cacheActivateCheckbox"
-          @change="saveCache()"
-        />
-      </div>
+      <CacheCheck />
     </div>
   </article>
 </template>
@@ -101,19 +92,11 @@ export default {
       "getStatus",
       "getSelectedColor",
       "getTogglePanel",
-      "getCacheActivate",
       "getPerson",
       "getPersonFirstName",
       "getPersonLastName",
     ]),
-    cacheActivateCheckbox: {
-      get() {
-        return this.getCacheActivate;
-      },
-      set(status) {
-        this.changeCacheActivate(status);
-      },
-    },
+
     firstNameForm: {
       get() {
         return this.getPersonFirstName;
@@ -141,10 +124,6 @@ export default {
         this.formDate = JSON.parse(localStorage.getItem("userDate"));
       }
 
-      this.changeCacheActivate(
-        JSON.parse(localStorage.getItem("userCache")) ?? false
-      );
-
       setTimeout(() => {
         !localStorage.getItem("userDate")
           ? (this.prepareDate(this.formDate),
@@ -158,7 +137,6 @@ export default {
       "changeStatus",
       "fetchCurentWeekEdtdata",
       "fetchEdtdata",
-      "changeCacheActivate",
       "changeSelectedColor",
       "changePerson",
     ]),
@@ -188,9 +166,6 @@ export default {
     saveName() {
       localStorage.setItem("user", JSON.stringify(this.getPerson));
       localStorage.setItem("userDate", JSON.stringify(this.formDate));
-    },
-    saveCache() {
-      localStorage.setItem("userCache", this.getCacheActivate);
     },
     resetStorage() {
       this.changePerson({
@@ -260,107 +235,6 @@ export default {
   align-items: center;
   align-content: center;
 }
-
-.cache-class {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: flex-start;
-  align-content: center;
-}
-
-.cache-class label {
-  font-family: "Quicksand", sans-serif;
-  margin: 0;
-  font-size: 14px;
-  margin-right: 5px;
-}
-
-/* pour la checkbox */
-
-.cache-class input[type="checkbox"] {
-  -webkit-appearance: none;
-  margin: 0;
-  margin-right: 3px;
-
-  padding: 7px;
-  border-radius: 4px;
-
-  display: inline-block;
-  position: relative;
-  transition: 0.3s;
-}
-
-.cache-class input[type="checkbox"]:hover {
-  cursor: pointer;
-}
-
-.cache-class .color1 {
-  border: 1px solid hsl(0, 70%, 75%);
-}
-
-.cache-class .color1:checked {
-  background-color: hsl(0, 70%, 85%);
-}
-
-.cache-class .color2 {
-  border: 1px solid hsl(60, 70%, 75%);
-}
-
-.cache-class .color2:checked {
-  background-color: hsl(60, 70%, 85%);
-}
-
-.cache-class .color3 {
-  border: 1px solid hsl(100, 70%, 75%);
-}
-
-.cache-class .color3:checked {
-  background-color: hsl(100, 70%, 85%);
-}
-
-.cache-class .color4 {
-  border: 1px solid hsl(170, 70%, 75%);
-}
-
-.cache-class .color4:checked {
-  background-color: hsl(170, 70%, 85%);
-}
-
-.cache-class .color5 {
-  border: 1px solid hsl(200, 70%, 75%);
-}
-
-.cache-class .color5:checked {
-  background-color: hsl(200, 70%, 85%);
-}
-
-.cache-class .color6 {
-  border: 1px solid hsl(250, 70%, 75%);
-}
-
-.cache-class .color6:checked {
-  background-color: hsl(250, 70%, 85%);
-}
-
-.cache-class .color7 {
-  border: 1px solid hsl(330, 70%, 75%);
-}
-
-.cache-class .color7:checked {
-  background-color: hsl(330, 70%, 85%);
-}
-
-.cache-class input[type="checkbox"]:checked:after {
-  content: ""; /* \2714 */
-  font-size: 8px;
-  position: absolute;
-  top: 0px;
-  left: 3px;
-  color: white;
-}
-
-/* */
 
 .person-form section input {
   font-family: "Quicksand", sans-serif;
